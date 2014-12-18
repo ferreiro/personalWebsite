8fecedf7bf9f639039324dc8298279768981d98d
Jorge Ferreiro <jogarc10@ucm.es>
2014-12-18 21:55:34 +0100

    New cool version!!!

--- website v.2/static/js/effects.js
+++ website v.2/static/js/effects.js
@@ -3,7 +3,6 @@
 		panel = $('.panel'),
 		footer = $('.footerWrap');
 	
-
 	initialEffect();
 
 	function initialEffect() { 
@@ -12,8 +11,29 @@
 		solidBg.slideDown(time);
 		background.delay(time).fadeIn(time + 200);
 		footer.delay(time).fadeIn(time / 2);
-		panel.delay(time).fadeIn(time + 200);
-		
-
+		panel.delay(time).fadeIn(time + 200); 
  	}
- 
+
+ 	$('#openPhoto').click(function() { 
+ 		$('.panelWrap').addClass("blur");
+ 		$('.modal').fadeIn(0);
+ 		$('.modal div').hide(0);
+ 		$('.modalProfileZoom').fadeIn(0);
+ 		return false;
+ 	});
+ 	
+ 	$('#openContact').click(function() { 
+ 		$('.panelWrap').addClass("blur");
+ 		$('.modal').fadeIn(0);
+ 		$('.modal div').hide(0);
+ 		$('.modalContact').fadeIn(0);
+ 		return false;
+ 	});
+
+ 	$('.modalClose').click(function() { 
+ 		$('.modal').hide(0); 
+ 		$('.panelWrap').removelass("blur");
+ 		$('.modalProfileZoom').hide(0);
+ 	});
+
+
